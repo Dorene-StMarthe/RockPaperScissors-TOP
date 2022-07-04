3d83f22c5b9dd3ea45f87844eddb5187daf80784
Dorene07 <dstmarth@gmail.com>
2022-07-04 12:02:31 -0400

    DOM practice

---
+++ JavaScript/DOM.js
@@ -0,0 +1,7 @@
+const container = document.querySelector("#container");
+const content = document.createElement("p");
+content.classList.add("redText");
+content.textContent = "Hey I'm red!"
+p.style.cssText = "color: red; background: white;";
+
+container.appendChild(redText);
