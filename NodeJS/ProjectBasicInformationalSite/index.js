54f30cfef80637ca8f2d193ae0a5bdbafda6cc31
Dorene07 <dstmarth@gmail.com>
2022-08-01 14:34:57 -0400

    installed packages

---
+++ NodeJS/ProjectBasicInformationalSite/index.js
@@ -0,0 +1,22 @@
+const http = require('http')
+
+const port = process.env.PORT || 8080
+
+// const mainIdx = {
+//     path: "index.html",
+//     method: "GET",
+// };
+
+
+
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<h1>Hello, World!</h1>')
+  })
+
+
+server.listen(port, ()=> {
+    console.log('Server running on port' + port )
+})
