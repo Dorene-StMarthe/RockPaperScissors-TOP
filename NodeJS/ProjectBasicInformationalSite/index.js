d143be69d8addc1fcde2a08452b62a76c8e6b7e5
Dorene07 <dstmarth@gmail.com>
2022-08-10 18:00:57 -0400

    added in file structure

--- NodeJS/ProjectBasicInformationalSite/index.js
+++ NodeJS/ProjectBasicInformationalSite/index.js
@@ -1,22 +1,20 @@
 const http = require('http')
 
-const port = process.env.PORT || 8080
-
-// const mainIdx = {
-//     path: "index.html",
-//     method: "GET",
-// };
+const app = http()
+const port = 8080
 
 
 
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.end('<h1>Hello, World!</h1>')
-  })
 
 
-server.listen(port, ()=> {
-    console.log('Server running on port' + port )
+app.get("/" ,(req, res) => {
+    res.render('index.html', {
+        })})
+   
+
+
+
+app.listen(port, ()=> {
+    console.log('Server running on port ' + port )
 })
