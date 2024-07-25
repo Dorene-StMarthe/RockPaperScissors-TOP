148ab38e762ddd79914c52e3bd5ad3af617892ba
Dorene07 <dstmarth@gmail.com>
2024-07-24 23:24:50 -0400

    logic to get human choice

--- RPS/script.js
+++ RPS/script.js
@@ -1,5 +1,5 @@
 
-function getComputerChoice() {
+/*function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     if (choice === 0) {
         return "rock";
@@ -9,4 +9,10 @@ function getComputerChoice() {
         return "scissors";
     }
 }
-    console.log(getComputerChoice());
+    //console.log(getComputerChoice());*/
+
+function getHumanChoice() {
+    let userInput = prompt("What would you choose? Rock, Paper or Scissors?");
+        return userInput;         
+}
+console.log(getHumanChoice());
