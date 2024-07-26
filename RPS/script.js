109e98aa63614e45c35f2905ea1241c47a3fc238
Dorene07 <dstmarth@gmail.com>
2024-07-26 16:37:54 -0400

    single round logic

--- RPS/script.js
+++ RPS/script.js
@@ -2,7 +2,7 @@ let humanScore = 0;
 let computerScore = 0;
 
 
-/*function getComputerChoice() {
+function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     if (choice === 0) {
         return "rock";
@@ -12,7 +12,7 @@ let computerScore = 0;
         return "scissors";
     }
 }
-    //console.log(getComputerChoice());*/
+    //console.log(getComputerChoice())
 
 function getHumanChoice() {
     let userInput = prompt("What would you choose? Rock, Paper or Scissors?");
@@ -20,3 +20,25 @@ function getHumanChoice() {
 }
 //console.log(getHumanChoice());
 
+function playRound(humanChoice, computerChoice){
+     humanChoice = getHumanChoice().toLowerCase();
+     computerChoice = getComputerChoice();
+        if (humanChoice === "rock" && computerChoice === "paper"){
+            return  "You lost. Paper beats rock."}
+        else if (humanChoice === "paper" && computerChoice === "scissors"){
+            return "You lost. Scissors beats paper"}
+        else if (humanChoice === "scissors" && computerChoice === "rock"){
+            return "You lost, Rock beats scissors"}
+        else if (humanChoice === computerChoice){
+            return "It's a tie! Play again!"
+        }
+
+        else {
+            return "YOU WON! " + humanChoice + " beats " + computerChoice + "!! YOUR SCORE:" + humanScore++ + " COMPUTER SCORE:" + computerScore + ".";
+        }}
+        
+        
+    
+
+
+console.log(playRound())
