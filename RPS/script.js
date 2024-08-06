997a61ce09ad4263c420709ba5e424e903b27320
Dorene07 <dstmarth@gmail.com>
2024-08-05 21:04:35 -0400

    configuring rounds

--- RPS/script.js
+++ RPS/script.js
@@ -2,6 +2,7 @@ let humanScore = 0;
 let computerScore = 0;
 
 
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     if (choice === 0) {
@@ -20,25 +21,56 @@ function getHumanChoice() {
 }
 //console.log(getHumanChoice());
 
-function playRound(humanChoice, computerChoice){
+/*function playRound(humanChoice, computerChoice){
      humanChoice = getHumanChoice().toLowerCase();
      computerChoice = getComputerChoice();
         if (humanChoice === "rock" && computerChoice === "paper"){
-            return  "You lost. Paper beats rock."}
+            return  "You lost. Paper beats rock. YOUR SCORE:" + humanScore + " COMPUTER SCORE: " + ++computerScore +".";}
         else if (humanChoice === "paper" && computerChoice === "scissors"){
-            return "You lost. Scissors beats paper."}
+            return "You lost. Scissors beats paper. YOUR SCORE:" + humanScore + " COMPUTER SCORE: " + ++computerScore +".";}
         else if (humanChoice === "scissors" && computerChoice === "rock"){
-            return "You lost, Rock beats scissors."}
+            return "You lost, Rock beats scissors. YOUR SCORE:" + humanScore + " COMPUTER SCORE: " + ++computerScore +".";}
         else if (humanChoice === computerChoice){
             return "It's a tie! Play again!"
         }
 
         else {
             return "YOU WON! " + humanChoice + " beats " + computerChoice + "!! YOUR SCORE:" + ++humanScore + " COMPUTER SCORE:" + computerScore + ".";
-        }}
+        }}*/
         
-        
-    
+//console.log(playRound())
 
+function playGame(){
+    function playRound(humanChoice, computerChoice){
+        humanChoice = getHumanChoice().toLowerCase();
+        computerChoice = getComputerChoice();
+           if (humanChoice === "rock" && computerChoice === "paper"){
+               return  "You lost. Paper beats rock. YOUR SCORE:" + humanScore + " COMPUTER SCORE: " + ++computerScore +".";}
+           else if (humanChoice === "paper" && computerChoice === "scissors"){
+               return "You lost. Scissors beats paper. YOUR SCORE:" + humanScore + " COMPUTER SCORE: " + ++computerScore +".";}
+           else if (humanChoice === "scissors" && computerChoice === "rock"){
+               return "You lost, Rock beats scissors. YOUR SCORE:" + humanScore + " COMPUTER SCORE: " + ++computerScore +".";}
+           else if (humanChoice === computerChoice){
+               return "It's a tie! Play again!"
+           }
+   
+           else {
+               return "YOU WON! " + humanChoice + " beats " + computerChoice + "!! YOUR SCORE:" + ++humanScore + " COMPUTER SCORE:" + computerScore + ".";
+           }}
+       
+       let round = playRound;
+     
+       
 
-console.log(playRound())
+round(1);
+console.log(playGame());
+round(2);
+console.log(playGame());
+round(3);
+console.log(playGame());
+round(4);
+console.log(playGame());
+round(5);
+console.log(playGame());
+};
+console.log(playGame())
