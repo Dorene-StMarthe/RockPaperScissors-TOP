799bffb56a2b10be0e8c2dbb15a3ebabf609d410
Dorene07 <dstmarth@gmail.com>
2024-08-14 22:04:35 -0400

    events and listeners

--- DOM/script.js
+++ DOM/script.js
@@ -32,3 +32,12 @@ para.classList.add("p");
 para.textContent ="ME TOO!"
 numThree.appendChild(para);
 
+const btn= document.querySelector("#btn");
+//btn.addEventListener("click", () => {
+    //alert("Hello World!");
+//})
+
+
+btn.addEventListener("click", function (e) {
+    e.target.style.background = "blue";  });
+  
